c78f0604c81c4b8f2bf72aa130e8f731e8e1ff36
EnzoRobaina <enzo.robaina@gmail.com>
2022-02-24 11:48:09 -0300

    Removes index.js from dist

--- dist/index.js
+++
@@ -1,6 +0,0 @@
-import DatePicker from "./components/DatePicker";
-import DateView from "./components/DateView";
-import MonthView from "./components/MonthView";
-
-export default DatePicker;
-export { DateView, MonthView };
